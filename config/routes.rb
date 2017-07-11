b79d9328245753efc84e9b04e90f59f8925d85cb
yo-no <spencerblake91@gmail.com>
2017-07-11 17:17:57 -0500

    set up admin namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :shows, except: [:index, :show]
+    get "/" => "admins#index"
+  end
+
   get '/' => 'pages#index'
+  get '/shows' => 'shows#index'
 end
--- db/migrate/20170711214741_create_shows.rb
+++ db/migrate/20170711214741_create_shows.rb
@@ -1,6 +1,10 @@
 class CreateShows < ActiveRecord::Migration[5.0]
   def change
     create_table :shows do |t|
+      t.date :date
+      t.text :description
+      t.text :location
+      t.text :url
 
       t.timestamps
     end
