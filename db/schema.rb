a0f75aaca1a5df58f24c1c44723600057dfa39c5
yo-no <spencerblake91@gmail.com>
2017-08-02 18:47:31 -0500

    added video gallery

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170711222054) do
+ActiveRecord::Schema.define(version: 20170802222227) do
 
   create_table "shows", force: :cascade do |t|
     t.date     "date"
@@ -27,6 +27,13 @@ ActiveRecord::Schema.define(version: 20170711222054) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+  end
+
+  create_table "videos", force: :cascade do |t|
+    t.string   "title"
+    t.string   "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
